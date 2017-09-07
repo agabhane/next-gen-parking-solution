f2e997c46fb3763c2f2a28d7f58b38be0c4ad6b7
Snehalkumar Mahale <snehalkumar.mahale@nice.com>
2017-09-07 14:11:36 +0530

    incorporated review comments

--- app/slots/slotModel.js
+++ app/slots/slotModel.js
@@ -1,17 +1,20 @@
 var mongoose = require('mongoose');
 
 var slotSchema = new mongoose.Schema({
-  name: {
-    type: String, required: true, unique: true
-  },
-  type: {
-    type: String, required: true
-  },
-  capacity: {
-    type: String, required: true    
-  }
+    companyId: {
+        type: String, required: true
+    },
+    name: {
+        type: String, required: true
+    },
+    type: {
+        type: String, required: true
+    },
+    capacity: {
+        type: Number, required: true
+    }
 }, {
     timestamps: true
-  });
+});
 
 module.exports = mongoose.model('Slot', slotSchema);
