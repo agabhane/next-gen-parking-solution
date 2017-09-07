3cefe78793387d51634b6f7548f8fd34dcb95baf
yjagdale <yashwant.jagdale@nice.com>
2017-09-07 17:05:13 +0530

    New Conf file

--- app/user/userModel.js
+++ app/user/userModel.js
@@ -1,32 +1,40 @@
 'use strict';
-var mongoose = require('mongoose');
-
+var mongoose = require('mongoose'),
+    validator = require('./validator');
 var userSchema = new mongoose.Schema({
-  name: {
-    type: String, required: true
-  },
-  companyId: {
-    type: String, required: true
-  },
-  email: {
-    type: String, required: true, unique: true
-  },
-  role: {
-    type: String,
-    enum: ['ADMIN', 'MANAGER', 'USER']
-  },
-  password: {
-    type: String
-  },
-  code: {
-    type: String
-  },
-  status: {
-    type: String, required: true,
-    enum: ['ACTIVE', 'INACTIVE']
-  }
+    name: {
+        type: String, required: true
+    },
+    companyId: {
+        type: String, required: true,
+        validate: {
+            isAsync: true,
+            validator: function (companyId, callBack) {
+                validator.companyValidator(companyId).then(function (isCompanyExist) {
+                    callBack(isCompanyExist, 'Company does not exist in DB');
+                })
+            }
+        }
+    },
+    email: {
+        type: String, required: true, unique: true
+    },
+    role: {
+        type: String,
+        enum: ['ADMIN', 'MANAGER', 'USER']
+    },
+    password: {
+        type: String
+    },
+    code: {
+        type: String
+    },
+    status: {
+        type: String, required: true,
+        enum: ['ACTIVE', 'INACTIVE']
+    }
 }, {
     timestamps: true
-  });
+});
 
 module.exports = mongoose.model('User', userSchema);
