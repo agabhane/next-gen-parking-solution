4a1f343866c5758fe0c42d9f57b5d931f85f6e41
yjagdale <yashwant.jagdale@nice.com>
2017-09-07 13:27:53 +0530

    Delete user

--- app/routes.js
+++ app/routes.js
@@ -3,29 +3,31 @@ var companyCtrl = require('./company/companyController');
 var userCtrl = require('./user/userController');
 
 module.exports = function (app) {
-  var apiRoutes = express.Router(),
-    companyRoutes = express.Router();
+    var apiRoutes = express.Router(),
+        companyRoutes = express.Router();
     userRoutes = express.Router();
 
 
-  //Company routes
-  apiRoutes.use('/company', companyRoutes);
-  companyRoutes.get('/', companyCtrl.getAllCompanies);
-  companyRoutes.post('/', companyCtrl.createCompany);
+    //Company routes
+    apiRoutes.use('/company', companyRoutes);
+    companyRoutes.get('/', companyCtrl.getAllCompanies);
+    companyRoutes.post('/', companyCtrl.createCompany);
 
-  //Slots routes
-
-  
-  //User routes
-
-  apiRoutes.use('/user', userRoutes);
-  userRoutes.get('/', userCtrl.getAllUsers);
-  userRoutes.post('/', userCtrl.createUser);
-  userRoutes.put('/:email', userCtrl.updateUser);
+    //Slots routes
 
 
-  //Transaction routes
-  
+    //User routes
 
-  app.use('/api', apiRoutes);
+    apiRoutes.use('/user', userRoutes);
+    userRoutes.get('/', userCtrl.getAllUsers);
+    userRoutes.get('/:email', userCtrl.getUser);
+    userRoutes.delete('/:email', userCtrl.deleteUser);
+    userRoutes.post('/', userCtrl.createUser);
+    userRoutes.put('/:email', userCtrl.updateUser);
+
+
+    //Transaction routes
+
+
+    app.use('/api', apiRoutes);
 }
